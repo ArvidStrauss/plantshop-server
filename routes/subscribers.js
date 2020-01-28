51565b52395e5d571dbbaac32ca01cd878a366d8
Arvid Strauss <avst@mms-t-systems.de>
2020-01-28 15:12:53 +0100

    finished tutorial

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -13,7 +13,9 @@ router.get("/", async (req, res) => {
 });
 
 // Get one subscriber
-router.get("/:id", (req, res) => {});
+router.get("/:id", getSubscriber, async (req, res) => {
+  res.json(res.subscriber);
+});
 
 // Create one subscriber
 router.post("/", async (req, res) => {
@@ -31,9 +33,44 @@ router.post("/", async (req, res) => {
 });
 
 // Update one subscriber
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name;
+  }
+
+  if (req.body.subscribedChannel != null) {
+    res.subscriber.subscribedChannel = req.body.subscribedChannel;
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save();
+    res.json(updatedSubscriber);
+  } catch {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // Delete one subscriber
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+    res.json({ message: "Deleted this Subscriber" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Middleware Function before :id requests
+async function getSubscriber(req, res, next) {
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json({ message: "Can't find subscriber" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.subscriber = subscriber;
+  next();
+}
 
 module.exports = router;
